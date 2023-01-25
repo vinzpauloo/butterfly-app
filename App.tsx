0964aefcc2baa3875e4c690eaf259b0557ba692b
jestoni-1bit <jestoni@1bit.com.ph>
2023-01-26 02:19:12 +0800

    BFLYAPP-32 - Initial global stack navigator layout. Cleanup App.tsx.

--- App.tsx
+++ App.tsx
@@ -1,152 +1,23 @@
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { StatusBar } from "react-native";
 import { useEffect } from "react";
 
-import Ionicons from "react-native-vector-icons/Ionicons";
-import Feather from "react-native-vector-icons/Feather";
-import Entypo from "react-native-vector-icons/Entypo";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { initializePusher } from "services/pusher";
 import { NavigationContainer } from "@react-navigation/native";
 
-import BottomTabs from "layouts/navigators/BottomTabs";
-import PortraitVideo from "layouts/PortraitVideo";
-import Search from "screens/Search";
-import SingleVideoScreen from "screens/SingleVideo";
-import { bottomNav } from "data/bottomNav";
+import StackNavigators from "layouts/navigators/StackNavigators";
 import { globalStyle } from "globalStyles";
-import { reelsVideos } from "data/reelsVideos";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   useEffect(() => {
-    initializePusher();
+    // initializePusher();
   }, []);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="关注"
-        screenOptions={{ animation: "slide_from_right" }}
-      >
-        <Stack.Screen name="关注" options={{ headerShown: false }}>
-          {(props) => <BottomTabs {...props} data={bottomNav} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="SingleVideo"
-          component={SingleVideoScreen}
-          options={({ navigation, route }: any) => ({
-            title: "",
-            headerStyle: {
-              backgroundColor: globalStyle.primaryColor,
-            },
-            headerLeft: () => (
-              <View
-                style={{
-                  flexDirection: "row",
-                }}
-              >
-                <Ionicons
-                  name="chevron-back-sharp"
-                  color="#fff"
-                  size={30}
-                  onPress={() => navigation.goBack()}
-                />
-                <Image source={route.params?.image} style={styles.image} />
-                <View>
-                  <Text style={styles.title}>{route.params?.title}</Text>
-                  <Text style={styles.subTitle}>{route.params?.subTitle}</Text>
-                </View>
-              </View>
-            ),
-            headerTitleStyle: {
-              color: "#fff",
-            },
-          })}
-        />
-        <Stack.Screen
-          name="Search"
-          component={Search}
-          options={({ navigation }: any) => ({
-            title: "",
-            headerStyle: {
-              backgroundColor: globalStyle.primaryColor,
-            },
-            headerLeft: () => (
-              <View
-                style={{
-                  flexDirection: "row",
-                }}
-              >
-                <Ionicons
-                  name="chevron-back-sharp"
-                  color="#fff"
-                  size={30}
-                  onPress={() => navigation.goBack()}
-                />
-                <View style={styles.searchInputContainer}>
-                  <Feather
-                    name="search"
-                    size={25}
-                    color="#aaa"
-                    style={styles.icon}
-                  />
-                  <TextInput placeholder="Search here" style={styles.input} />
-                  <Entypo
-                    name="circle-with-cross"
-                    size={18}
-                    color="#aaa"
-                    style={styles.icon}
-                  />
-                </View>
-              </View>
-            ),
-            headerTitleStyle: {
-              color: "#fff",
-            },
-          })}
-        />
-        <Stack.Screen name="VlogScreen" options={{ headerShown: false }}>
-          {(props) => (
-            <PortraitVideo reelsVideos={reelsVideos} hasBackButton={true} />
-          )}
-        </Stack.Screen>
-      </Stack.Navigator>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={globalStyle.primaryColor}
+      />
+      <StackNavigators />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    height: 30,
-    width: 30,
-    borderRadius: 15,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  title: {
-    color: "#fff",
-  },
-  subTitle: {
-    color: "#999",
-  },
-  searchInputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    width: 250,
-    marginHorizontal: 10,
-  },
-  icon: {
-    marginHorizontal: 6,
-  },
-  input: {
-    width: 180,
-  },
-});
--- src/layouts/navigators/BottomTabs.tsx
+++ src/layouts/navigators/BottomTabs.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from "react-native";
+import {} from "react-native";
 import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -9,36 +9,30 @@ const BottomTab = createBottomTabNavigator();
 
 const BottomTabs = ({ data }) => {
   return (
-    <>
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={globalStyle.primaryColor}
-      />
-      <BottomTab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: globalStyle.secondaryColor,
-          tabBarInactiveTintColor: "#fff",
-          tabBarStyle: {
-            backgroundColor: "#262632",
-            paddingBottom: 10,
-            height: 60,
-          },
-        }}
-      >
-        {data?.map((item, index) => (
-          <BottomTab.Screen
-            key={index}
-            name={item.name}
-            component={item.component}
-            options={{
-              tabBarLabel: item.label,
-              tabBarIcon: item.icon,
-            }}
-          />
-        ))}
-      </BottomTab.Navigator>
-    </>
+    <BottomTab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: globalStyle.secondaryColor,
+        tabBarInactiveTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#262632",
+          paddingBottom: 10,
+          height: 60,
+        },
+      }}
+    >
+      {data?.map((item, index) => (
+        <BottomTab.Screen
+          key={index}
+          name={item.name}
+          component={item.component}
+          options={{
+            tabBarLabel: item.label,
+            tabBarIcon: item.icon,
+          }}
+        />
+      ))}
+    </BottomTab.Navigator>
   );
 };
 

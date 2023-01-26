4e38b65406dd1c3610e2a5a6b846dfdf98f11077
Mark <mark@1bit.com.ph>
2023-01-26 18:13:54 +0800

    BFLYAPP-81 - Display settings icon and bell icon

--- src/screens/AccountTab.tsx
+++ src/screens/AccountTab.tsx
@@ -1,16 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-type Props = {};
+import Fontisto from "react-native-vector-icons/Fontisto";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-const Account = (props: Props) => {
+import Container from "components/Container";
+
+const Header = () => {
   return (
-    <View>
-      <Text>Account</Text>
+    <View style={styles.header}>
+      <Fontisto name="bell" color="#fff" size={25} style={styles.icon} />
+      <Ionicons
+        name="settings-outline"
+        color="#fff"
+        size={25}
+        style={styles.icon}
+      />
     </View>
+  );
+};
+
+const Account = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
   );
 };
 
 export default Account;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  icon: {
+    marginHorizontal: 10,
+  },
+});

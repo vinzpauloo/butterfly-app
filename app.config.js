e2f3a12fcb4dd63aeb7b8968e920e0aad315709f
jestoni-1bit <jestoni@1bit.com.ph>
2023-01-26 12:40:19 +0800

    Setup Expo config for building separate development & preview versions

---
+++ app.config.js
@@ -0,0 +1,40 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+
+export default {
+  name: IS_DEV ? "Butterfly App (Dev)" : "Butterfly App (Preview)",
+  slug: "butterfly-mobile-app-fe",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+    package: IS_DEV
+      ? "com.butterflymobileappfe.dev"
+      : "com.butterflymobileappfe",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "e96e6e4b-e8b5-42ef-842b-302e50e14beb",
+    },
+  },
+  owner: "1bit-frontend",
+};

2770bb6dc535f82856bb170c74e0f680c8b55a28
jestoni-1bit <jestoni@1bit.com.ph>
2023-01-27 14:56:14 +0800

    BFLYAPP-209 - Single Section Screen header

---
+++ src/screens/SingleSection/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import Container from "components/Container";
+
+const SingleSectionScreen = ({}) => {
+  return (
+    <Container>
+      <Text>SingleSectionScreen</Text>
+    </Container>
+  );
+};
+
+export default SingleSectionScreen;
+
+const styles = StyleSheet.create({});

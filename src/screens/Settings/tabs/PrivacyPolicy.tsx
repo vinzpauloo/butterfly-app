5d234492eda8609294f227f2a19ff1c012e062e2
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-02-09 15:26:08 +0800

    BFLYAPP-266 and 267 - Updated PrivacyPolicy and ServiceProvisions

--- src/screens/Settings/tabs/PrivacyPolicy.tsx
+++ src/screens/Settings/tabs/PrivacyPolicy.tsx
@@ -1,21 +1,47 @@
 import React from "react";
-
+import {Image, SafeAreaView, Text, View, StyleSheet} from "react-native";
 import { WebView } from 'react-native-webview'
 
 import UserProfileSettingsHeader from "components/UserProfileSettingsHeader";
 import {privacyPolicyData} from "data/privacyPolicyData";
+import Logo from 'assets/images/butterfly.png'
 const PrivacyPolicy = () => {
 
   return (
-      <>
+      <SafeAreaView style={styles.container}>
         <UserProfileSettingsHeader title={null} btnRight={null}/>
+          <View style={styles.titleContainer}>
+              <Image source={Logo} style={styles.image}/>
+              <Text style={styles.title}>Privacy Policy</Text>
+          </View>
         <WebView
             source={{ html: privacyPolicyData.map((item) => item.chinese.description).join('') }}
-            style={{flex: 1}}
+            style={styles.htmlStyle}
             scalesPageToFit={false}
         />
-      </>
+      </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#191d26'
+    },
+    titleContainer: {
+        alignItems: 'center'
+    },
+    image: {
+        width: 80, height: 80
+    },
+    title: {
+        color: '#FFF',
+        fontWeight: '900'
+    },
+    htmlStyle: {
+        flex: 1,
+        backgroundColor: '#191d26'
+    }
+})
 
 export default PrivacyPolicy;
--- src/screens/Settings/tabs/ServiceProvisions.tsx
+++ src/screens/Settings/tabs/ServiceProvisions.tsx
@@ -1,20 +1,45 @@
 import React from "react";
-
+import {Image, SafeAreaView, StyleSheet, Text, View} from "react-native";
 import { WebView } from 'react-native-webview'
 
 import UserProfileSettingsHeader from "components/UserProfileSettingsHeader";
 import {serviceProvisionsData} from "data/serviceProvisionsData";
-
+import Logo from 'assets/images/butterfly.png'
 const ServiceProvisions = () => {
     return (
-        <>
+        <SafeAreaView style={styles.container}>
             <UserProfileSettingsHeader title={null} btnRight={null}/>
+            <View style={styles.titleContainer}>
+                <Image source={Logo} style={styles.image}/>
+                <Text style={styles.title}>Service Provisions</Text>
+            </View>
             <WebView
-                source={{ html: serviceProvisionsData.map((item) => item.chinese.description).join('') }}
-                style={{flex: 1}}
+                source={{ html: serviceProvisionsData.map((item) => item.english.description).join('')}}
+                style={styles.htmlStyle}
                 scalesPageToFit={false}
             />
-        </>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#191d26'
+    },
+    titleContainer: {
+        alignItems: 'center'
+    },
+    image: {
+        width: 80, height: 80
+    },
+    title: {
+        color: '#FFF',
+        fontWeight: '900'
+    },
+    htmlStyle: {
+        flex: 1,
+        backgroundColor: '#191d26'
+    }
+})
 export default ServiceProvisions;

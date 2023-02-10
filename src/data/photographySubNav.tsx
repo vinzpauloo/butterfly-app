0d3128c32e1e2dae5a8e498dfcb86cd724f2e3d3
jestoni-1bit <jestoni@1bit.com.ph>
2023-02-10 16:33:12 +0800

    Code cleanup

--- src/data/photographySubNav.tsx
+++ src/data/photographySubNav.tsx
@@ -1,21 +1,21 @@
-import { DynamicScreen } from "screens/Home/tabs/Home";
 import MasonryPhotos from "layouts/MasonryPhotos";
 
-
 export const photographySubNav = {
-	initialRoute: "综合",
-	screens: [
-		{
-			name: "综合",
-			component: (props) => <MasonryPhotos {...props} title={"comprehensive"} />,
-		},
-		{
-			name: "最热",
-			component: (props) => <MasonryPhotos {...props} title={"hottest"} />,
-		},
-		{
-			name: "最新",
-			component: (props) => <MasonryPhotos {...props} title={"newest"} />,
-		},
-	],
+  initialRoute: "综合",
+  screens: [
+    {
+      name: "综合",
+      component: (props) => (
+        <MasonryPhotos {...props} title={"comprehensive"} />
+      ),
+    },
+    {
+      name: "最热",
+      component: (props) => <MasonryPhotos {...props} title={"hottest"} />,
+    },
+    {
+      name: "最新",
+      component: (props) => <MasonryPhotos {...props} title={"newest"} />,
+    },
+  ],
 };

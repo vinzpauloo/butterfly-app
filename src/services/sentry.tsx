3742dc6cc3eccdf95303f6dedc367e683058dd13
VincentPaul <oo.vincentpaul@1bit.com.ph>
2023-02-14 19:07:43 +0800

    Added notes/instructions on sentry.tsx

--- src/services/sentry.tsx
+++ src/services/sentry.tsx
@@ -17,3 +17,27 @@ export const captureError = (error, route) => {
     Sentry.Native.captureMessage(`ROUTE/PAGE: "${route}", ERROR MESSAGE: "${e}"`)
     Sentry.Native.captureException(error, route)
 }
+
+// How to use:
+// 1. In your target component, create a variable 'route':
+//
+//    const route = useRoute<any>()
+//
+// 2. Import the functions captureError and captureSuccess
+//
+// 3. In your 'useQuery', 'useQueries', you may attach the function captureSuccess and captureError on your 'onSuccess' and 'onError', please see the code below as an example:
+
+    // const { example } = useQuery({
+    //     queryKey: ["exampleKey"],
+    //     queryFn: hookFunction,
+    //     onSuccess:
+    //         //Your code here...
+    //         captureSuccess(route.name)
+    //     },
+    //     onError: (error) => {
+    //         captureError(error, route.name)
+    //     },
+    // });
+
+// IMPORTANT NOTE: Please make sure to download the ".env" file from our team chat for the Sentry "dsn"
+

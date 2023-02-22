ed90b28b28c3f652a5862a8e27a1560d719e40a4
jestoni-1bit <jestoni@1bit.com.ph>
2023-02-22 14:39:58 +0800

    Updated Customer Service: Added Favorite-related APIs Added Follow-related APIs

--- src/services/api/CustomerService.tsx
+++ src/services/api/CustomerService.tsx
@@ -6,6 +6,19 @@ interface IFavoritesOrWatchedHistory {
   type: "favorites" | "watched_history";
 }
 
+interface ICommons {
+  customer_id: string;
+  site_id?: number;
+}
+
+interface IFollow extends ICommons {
+  user_id: string /* Content creator ID */;
+}
+
+interface IFavorite extends ICommons {
+  foreign_id: string /* Work ID */;
+}
+
 const CustomerService = () => {
   const getCustomerById = (customer_id: string) => {
     return request({
@@ -29,7 +42,63 @@ const CustomerService = () => {
     });
   };
 
-  return { getCustomerById, getFollowedCreators, getFavoritesOrWatchedHistory };
+  const followChecker = (data: IFollow) => {
+    return request({
+      url: "/customer/follow-checker",
+      method: "POST",
+      data,
+    });
+  };
+
+  const followCreator = (data: IFollow) => {
+    return request({
+      url: "/customer/follow-creator",
+      method: "POST",
+      data,
+    });
+  };
+
+  const unfollowCreator = (data: IFollow) => {
+    return request({
+      url: `/customer/unfollow-creator/${data.user_id}/${data.customer_id}`,
+      method: "DELETE",
+    });
+  };
+
+  const favoriteChecker = (data: IFavorite) => {
+    return request({
+      url: "/customer/favorite-checker",
+      method: "POST",
+      data,
+    });
+  };
+
+  const favoriteVideo = (data: IFavorite) => {
+    return request({
+      url: "/customer/save-favorite",
+      method: "POST",
+      data,
+    });
+  };
+
+  const unfavoriteVideo = (data: IFavorite) => {
+    return request({
+      url: `/customer/remove-favorite/${data.foreign_id}/${data.customer_id}`,
+      method: "DELETE",
+    });
+  };
+
+  return {
+    getCustomerById,
+    getFollowedCreators,
+    getFavoritesOrWatchedHistory,
+    followChecker,
+    followCreator,
+    unfollowCreator,
+    favoriteChecker,
+    favoriteVideo,
+    unfavoriteVideo,
+  };
 };
 
 export default CustomerService;

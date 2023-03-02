945cfc8ec1184102b4dbeb5bd2c504267849bc37
Mark <mark@1bit.com.ph>
2023-03-02 17:23:03 +0800

    BFLYAPP-456 - PUT Like feed

--- src/screens/SingleTag/tabs/Moment.tsx
+++ src/screens/SingleTag/tabs/Moment.tsx
@@ -4,15 +4,17 @@ import React, { useState } from "react";
 import CommentListSkeleton from "components/skeletons/CommentListSkeleton";
 import Feeds from "components/feed/Feeds";
 import { Feeds as FeedsAPI } from "hooks/useFeeds";
+import { useIsFocused } from "@react-navigation/native";
 import { useQuery } from "@tanstack/react-query";
 
 const Moment = ({ userId }) => {
   const { getFeedsByTag } = FeedsAPI();
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+  const isFocus = useIsFocused();
 
   const { isLoading } = useQuery({
-    queryKey: ["feedTab", userId],
+    queryKey: [`feedTab${isFocus}`, userId],
     queryFn: () =>
       getFeedsByTag({
         tag: "Asian", // will change later the Asian have the only one that have data for now
@@ -25,6 +27,7 @@ const Moment = ({ userId }) => {
     onSuccess: (data) => {
       setData(data.data);
     },
+    enabled: isFocus,
   });
 
   if (isLoading) {

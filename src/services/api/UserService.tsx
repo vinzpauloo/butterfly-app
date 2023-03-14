cac67f979e37bdec277ba115cdcc3d2127ee6f96
jestoni-1bit <jestoni@1bit.com.ph>
2023-03-14 20:48:15 +0800

    REFACTOR: Update getAllFollowerList params

--- src/services/api/UserService.tsx
+++ src/services/api/UserService.tsx
@@ -2,49 +2,54 @@ import { getHeaders } from "lib/cryptoJs";
 import request from "lib/request";
 
 interface IUserParams {
-	data: {
-		user_id?: number
-		paginate?: number
-		page?: number
-	}
-	token: string;
+  data: {
+    user_id?: number;
+    paginate?: number;
+    page?: number;
+  };
+  token: string;
 }
 
 const UserService = () => {
-	const getSpecificContentCreator = (params: IUserParams) => {
-		return request({
-			headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
-			url: `/content-creators/${params.data.user_id}`,
-			method: "GET",
-		});
-	};
+  const getSpecificContentCreator = (params: IUserParams) => {
+    return request({
+      headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
+      url: `/content-creators/${params.data.user_id}`,
+      method: "GET",
+    });
+  };
 
-	const getAllFollowerList = (params: IUserParams) => {
-		return request({
-			headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
-			url: "/followers/list",
-			method: "GET",
-			params,
-		});
-	};
+  const getAllFollowerList = (params: IUserParams) => {
+    return request({
+      headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
+      url: "/followers/list",
+      method: "GET",
+      params: params.data,
+    });
+  };
 
-	const getFollowersCount = (params: IUserParams) => {
-		return request({
-			headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
-			url: `/followers/count/${params.data.user_id}`,
-			method: "GET",
-		});
-	};
+  const getFollowersCount = (params: IUserParams) => {
+    return request({
+      headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
+      url: `/followers/count/${params.data.user_id}`,
+      method: "GET",
+    });
+  };
 
-	const getDonatorsCount = (params: IUserParams) => {
-		return request({
-			headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
-			url: `/customers/total-donators/${params.data.user_id}`,
-			method: "GET",
-		});
-	};
+  const getDonatorsCount = (params: IUserParams) => {
+    return request({
+      headers: { ...getHeaders(), Authorization: `Bearer ${params.token}` },
+      url: `/customers/total-donators/${params.data.user_id}`,
+      method: "GET",
+    });
+  };
 
-	return { getSpecificContentCreator, getAllFollowerList, getFollowersCount, getDonatorsCount };
+  return {
+    getSpecificContentCreator,
+    getAllFollowerList,
+    getFollowersCount,
+    getDonatorsCount,
+  };
 };
 
 export default UserService;

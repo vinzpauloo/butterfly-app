5fc4b4280211017452818a3965c51f5ad8ddca90
Mark <mark@1bit.com.ph>
2023-03-21 16:48:20 +0800

    add translation in comment

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,100 +1,131 @@
-import React, { useState } from 'react'
-import { Pressable, StyleSheet, Text } from 'react-native'
-import { HStack, Avatar, VStack } from 'native-base';
-import { GLOBAL_COLORS } from 'global';
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { HStack, Avatar, VStack } from "native-base";
+import { GLOBAL_COLORS } from "global";
 
-import { commentGlobalStore } from "../zustand/commentGlobalStore"
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import { FlashList } from '@shopify/flash-list';
+import { commentGlobalStore } from "../zustand/commentGlobalStore";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import { FlashList } from "@shopify/flash-list";
+import { translationStore } from "../zustand/translationStore";
 
 type repliesDataType = {
-	customerId: string;
-	username: string;
-	photo: string;
-	comment: string;
+  customerId: string;
+  username: string;
+  photo: string;
+  comment: string;
 };
 
 type commentItemProps = {
-	commentID: string;
-	comment: string;
-	username: string;
-	photo: string;
-	replies: repliesDataType[];
+  commentID: string;
+  comment: string;
+  username: string;
+  photo: string;
+  replies: repliesDataType[];
 };
 
 const CommentItem = (props: commentItemProps) => {
-	const [repliesIsShown, setrepliesIsShown] = useState(false);
-	const [amountOfCommentShown, setAmountOfCommentShown] = useState(10);
+  const translations = translationStore((state) => state.translations);
+  const [repliesIsShown, setrepliesIsShown] = useState(false);
+  const [amountOfCommentShown, setAmountOfCommentShown] = useState(10);
 
-	// subscribe to comment global store
-	const [setIsOnReplyMode, setUserNameToReplyTo, setCommentIDToReplyTo, globalTextInputRef] = commentGlobalStore(
-		(state) => [state.setIsOnReplyMode, state.setUserNameToReplyTo, state.setCommentIDToReplyTo, state.globalTextInputRef],
-	)
+  // subscribe to comment global store
+  const [
+    setIsOnReplyMode,
+    setUserNameToReplyTo,
+    setCommentIDToReplyTo,
+    globalTextInputRef,
+  ] = commentGlobalStore((state) => [
+    state.setIsOnReplyMode,
+    state.setUserNameToReplyTo,
+    state.setCommentIDToReplyTo,
+    state.globalTextInputRef,
+  ]);
 
-	function enableReplyMode() {
-		setIsOnReplyMode(true)
-		globalTextInputRef.current.focus()
-		setUserNameToReplyTo(props.username)
-		setCommentIDToReplyTo(props.commentID)
-	}
+  function enableReplyMode() {
+    setIsOnReplyMode(true);
+    globalTextInputRef.current.focus();
+    setUserNameToReplyTo(props.username);
+    setCommentIDToReplyTo(props.commentID);
+  }
 
-	return (
-		<HStack space={2}>
-			<Avatar size={42} source={{ uri: props.photo }} />
-			<VStack space={1} flex={1} px={2}>
-				<Text style={styles.whiteText}>{props.username}</Text>
-				<Text style={styles.whiteText}>{props.comment}</Text>
-				<Pressable style={styles.interactable} onPress={enableReplyMode}>
-					<HStack space={1.5} alignItems="center">
-						<Fontisto name="commenting" color={GLOBAL_COLORS.inactiveTextColor} size={14} />
-						<Text style={styles.secondaryText}>回复</Text>
-					</HStack>
-				</Pressable>
-				{props?.replies?.length > 0 ?
-					<Pressable style={styles.interactable} onPress={() => {setrepliesIsShown(!repliesIsShown)}}>
-						<Text style={styles.secondaryColor}>查看 {props?.replies?.length} 则回复</Text>
-					</Pressable> : null}
-				{repliesIsShown && props?.replies?.length > 0 ? 
-					<VStack flex={1} space={4} minH={49}>
-						<FlashList
-							estimatedItemSize={50}
-							data={props?.replies?.slice(0, amountOfCommentShown)}
-							removeClippedSubviews={true}
-							renderItem={({ item }: any) =>
-								<HStack space={2} my={2}>
-									<Avatar size={42} source={{ uri: item.photo }} />
-									<VStack space={1} px={2} flex={1}>
-										<Text style={styles.whiteText}>{item.username}</Text>
-										<Text style={styles.whiteText}>{item.comment}</Text>
-									</VStack>
-								</HStack>
-							} />
-							{props?.replies?.length >= 10 && amountOfCommentShown < props?.replies?.length ?
-								<Pressable onPress={() => setAmountOfCommentShown((prev) => prev + 10)}>
-									<Text style={styles.loadMoreComments}>更多裝載</Text>
-								</Pressable> : null}
-					</VStack> : null}
-			</VStack>
-		</HStack>
-	);
+  return (
+    <HStack space={2}>
+      <Avatar size={42} source={{ uri: props.photo }} />
+      <VStack space={1} flex={1} px={2}>
+        <Text style={styles.whiteText}>{props.username}</Text>
+        <Text style={styles.whiteText}>{props.comment}</Text>
+        <Pressable style={styles.interactable} onPress={enableReplyMode}>
+          <HStack space={1.5} alignItems="center">
+            <Fontisto
+              name="commenting"
+              color={GLOBAL_COLORS.inactiveTextColor}
+              size={14}
+            />
+            <Text style={styles.secondaryText}>{translations.reply}</Text>
+          </HStack>
+        </Pressable>
+        {props?.replies?.length > 0 ? (
+          <Pressable
+            style={styles.interactable}
+            onPress={() => {
+              setrepliesIsShown(!repliesIsShown);
+            }}
+          >
+            <Text style={styles.secondaryColor}>
+              {translations.check} {props?.replies?.length}{" "}
+              {translations.thenReply}
+            </Text>
+          </Pressable>
+        ) : null}
+        {repliesIsShown && props?.replies?.length > 0 ? (
+          <VStack flex={1} space={4} minH={49}>
+            <FlashList
+              estimatedItemSize={50}
+              data={props?.replies?.slice(0, amountOfCommentShown)}
+              removeClippedSubviews={true}
+              renderItem={({ item }: any) => (
+                <HStack space={2} my={2}>
+                  <Avatar size={42} source={{ uri: item.photo }} />
+                  <VStack space={1} px={2} flex={1}>
+                    <Text style={styles.whiteText}>{item.username}</Text>
+                    <Text style={styles.whiteText}>{item.comment}</Text>
+                  </VStack>
+                </HStack>
+              )}
+            />
+            {props?.replies?.length >= 10 &&
+            amountOfCommentShown < props?.replies?.length ? (
+              <Pressable
+                onPress={() => setAmountOfCommentShown((prev) => prev + 10)}
+              >
+                <Text style={styles.loadMoreComments}>
+                  {translations.moreLoading}
+                </Text>
+              </Pressable>
+            ) : null}
+          </VStack>
+        ) : null}
+      </VStack>
+    </HStack>
+  );
 };
-export default CommentItem
+export default CommentItem;
 
 const styles = StyleSheet.create({
-	whiteText: {
-		color: GLOBAL_COLORS.primaryTextColor,
-	},
-	secondaryColor: {
-		color: GLOBAL_COLORS.secondaryColor,
-	},
-	secondaryText: {
-		color: GLOBAL_COLORS.inactiveTextColor,
-	},
-	loadMoreComments: {
-		textAlign: "center",
-		color: GLOBAL_COLORS.inactiveTextColor,
-	},
-	interactable: {
-		paddingVertical: 8,
-	}
-})
+  whiteText: {
+    color: GLOBAL_COLORS.primaryTextColor,
+  },
+  secondaryColor: {
+    color: GLOBAL_COLORS.secondaryColor,
+  },
+  secondaryText: {
+    color: GLOBAL_COLORS.inactiveTextColor,
+  },
+  loadMoreComments: {
+    textAlign: "center",
+    color: GLOBAL_COLORS.inactiveTextColor,
+  },
+  interactable: {
+    paddingVertical: 8,
+  },
+});
--- src/features/commentList/index.tsx
+++ src/features/commentList/index.tsx
@@ -9,145 +9,182 @@ import CommentsService from "services/api/CommentsService";
 import CommentListSkeleton from "../../components/skeletons/CommentListSkeleton";
 import FeedItemSkeleton from "components/skeletons/FeedItemSkeleton";
 
-import { GLOBAL_COLORS } from "global";
-import { useQuery } from "@tanstack/react-query";
-import { commentGlobalStore } from "../../zustand/commentGlobalStore";
-import Loading from "components/Loading";
-import { userStore } from "../../zustand/userStore";
 import Container from "components/Container";
+import Loading from "components/Loading";
+import { commentGlobalStore } from "../../zustand/commentGlobalStore";
+import { GLOBAL_COLORS } from "global";
 import { Tabs } from "react-native-collapsible-tab-view";
+import { translationStore } from "../../zustand/translationStore";
+import { useQuery } from "@tanstack/react-query";
+import { userStore } from "../../zustand/userStore";
 
 type CommentListProps = {
-	isFromFeed?: boolean
-	isFromSingleVideo?: boolean
-	workID: string
-	customHeaderComponent?: React.ReactComponentElement<any>
-}
+  isFromFeed?: boolean;
+  isFromSingleVideo?: boolean;
+  workID: string;
+  customHeaderComponent?: React.ReactComponentElement<any>;
+};
 
 const CommentList = (props: CommentListProps) => {
-	const token = userStore((store) => store.api_token);
-	
-	const [amountOfCommentsToGet, setAmountOfCommentsToGet] = useState(10);
-	const commentListRef = useRef<any>()
-	const { getComments } = CommentsService();
-	const { isLoading, data, isRefetching } = useQuery({
+  const token = userStore((store) => store.api_token);
+  const translations = translationStore((store) => store.translations);
+  const [amountOfCommentsToGet, setAmountOfCommentsToGet] = useState(10);
+  const commentListRef = useRef<any>();
+  const { getComments } = CommentsService();
+  const { isLoading, data, isRefetching } = useQuery({
     queryKey: ["workComments", props.workID, amountOfCommentsToGet],
-		queryFn: () => getComments({
-			data: {
-				foreign_id: props.workID,
-				paginate: amountOfCommentsToGet,
-				type: "comments",
-			},
-			token: token
-		}),
-		onSuccess: () => {
-			console.log("=== commentlist fetched from backend! ===")
-		},
-		onError: (error) => {
-			alert(error);
-		},
-	});
+    queryFn: () =>
+      getComments({
+        data: {
+          foreign_id: props.workID,
+          paginate: amountOfCommentsToGet,
+          type: "comments",
+        },
+        token: token,
+      }),
+    onSuccess: () => {
+      console.log("=== commentlist fetched from backend! ===");
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
 
-	const onScrollToEnd = () => {
-    if (data?.next_page_url !== null) setAmountOfCommentsToGet((prev) => prev + 10)
-	}
+  const onScrollToEnd = () => {
+    if (data?.next_page_url !== null)
+      setAmountOfCommentsToGet((prev) => prev + 10);
+  };
 
-	// subscribe to comment global store
-	const [setGlobalCommentListRef] = commentGlobalStore((state) => [state.setGlobalCommentListRef])
+  // subscribe to comment global store
+  const [setGlobalCommentListRef] = commentGlobalStore((state) => [
+    state.setGlobalCommentListRef,
+  ]);
 
-	useEffect(() => {
-		setGlobalCommentListRef(commentListRef)
-		return () => { setGlobalCommentListRef(null) }
-	}, [])
-	
-	if (props.isFromSingleVideo) {
-		return (
-			<Container>
-				{amountOfCommentsToGet === 10 && isLoading ?  
-					<Tabs.FlatList
-						style={{ padding: 12 }}
-						data={null}
-						renderItem={null}
-						ListEmptyComponent={<CommentListSkeleton />}
-					/> :
-					<Tabs.FlatList
-						style={{padding: 12}}
-						ref={commentListRef}
-						data={data?.data}
-						ListHeaderComponent={<Text style={styles.commentHeader}>全部评论 {data?.total}</Text>}
-						ListFooterComponent={data?.next_page_url === null ? <BottomMessage isFromSingleVideo /> : null}
-						keyExtractor={(_, index) => "" + index}
-						renderItem={({ item }: any) => (
-							<CommentItem
-								commentID={item.comment_id}
-								comment={item.comment}
-								username={item.username}
-								photo={item.photo}
-								replies={item.replies}
-							/>)}
-						ItemSeparatorComponent={() => <Divider color="#999" style={styles.divider} />}
-						onEndReachedThreshold={0.1}
-						onEndReached={onScrollToEnd}
-					/>}
-				{/* temporary solution for loading - needs improvement */}
-				{(isLoading || isRefetching) && amountOfCommentsToGet > 10 ? <Loading /> : null}
-			</Container>
-		)
-	}
-	
-	else return (
-		<View style={styles.commentsContainer}>
-			{amountOfCommentsToGet === 10 && isLoading ? 
-				props.isFromFeed ? 
-				<>
-						<FeedItemSkeleton />
-						<CommentListSkeleton />
-				</>
-					: <CommentListSkeleton /> 
-				:
-				<>
-					<FlashList
-						ref={commentListRef}
-						removeClippedSubviews={true}
-						estimatedItemSize={117}
-						showsVerticalScrollIndicator={false}
-						data={data?.data}
-						ListHeaderComponent={<>{props.customHeaderComponent}<Text style={styles.commentHeader}>全部评论 {data?.total}</Text></>}
-						ListFooterComponent={data?.next_page_url === null ? <BottomMessage /> : null}
-						keyExtractor={(_, index) => "" + index}
-						renderItem={({ item }: any) => (
-							<CommentItem
-								commentID={item.comment_id}
-								comment={item.comment}
-								username={item.username}
-								photo={item.photo}
-								replies={item.replies}
-							/>)}
-						ItemSeparatorComponent={() => <Divider color="#999" style={styles.divider} />}
-						onEndReachedThreshold={0.1}
-						onEndReached={onScrollToEnd}
-					/>
-					{/* temporary solution for loading - needs improvement */}
-					{(isLoading || isRefetching) && amountOfCommentsToGet > 10 ? <Loading /> : null}
-				</>
-			}
-		</View>
-	);
+  useEffect(() => {
+    setGlobalCommentListRef(commentListRef);
+    return () => {
+      setGlobalCommentListRef(null);
+    };
+  }, []);
+
+  if (props.isFromSingleVideo) {
+    return (
+      <Container>
+        {amountOfCommentsToGet === 10 && isLoading ? (
+          <Tabs.FlatList
+            style={{ padding: 12 }}
+            data={null}
+            renderItem={null}
+            ListEmptyComponent={<CommentListSkeleton />}
+          />
+        ) : (
+          <Tabs.FlatList
+            style={{ padding: 12 }}
+            ref={commentListRef}
+            data={data?.data}
+            ListHeaderComponent={
+              <Text style={styles.commentHeader}>
+                {translations.allComments} {data?.total}
+              </Text>
+            }
+            ListFooterComponent={
+              data?.next_page_url === null ? (
+                <BottomMessage isFromSingleVideo />
+              ) : null
+            }
+            keyExtractor={(_, index) => "" + index}
+            renderItem={({ item }: any) => (
+              <CommentItem
+                commentID={item.comment_id}
+                comment={item.comment}
+                username={item.username}
+                photo={item.photo}
+                replies={item.replies}
+              />
+            )}
+            ItemSeparatorComponent={() => (
+              <Divider color="#999" style={styles.divider} />
+            )}
+            onEndReachedThreshold={0.1}
+            onEndReached={onScrollToEnd}
+          />
+        )}
+        {/* temporary solution for loading - needs improvement */}
+        {(isLoading || isRefetching) && amountOfCommentsToGet > 10 ? (
+          <Loading />
+        ) : null}
+      </Container>
+    );
+  } else
+    return (
+      <View style={styles.commentsContainer}>
+        {amountOfCommentsToGet === 10 && isLoading ? (
+          props.isFromFeed ? (
+            <>
+              <FeedItemSkeleton />
+              <CommentListSkeleton />
+            </>
+          ) : (
+            <CommentListSkeleton />
+          )
+        ) : (
+          <>
+            <FlashList
+              ref={commentListRef}
+              removeClippedSubviews={true}
+              estimatedItemSize={117}
+              showsVerticalScrollIndicator={false}
+              data={data?.data}
+              ListHeaderComponent={
+                <>
+                  {props.customHeaderComponent}
+                  <Text style={styles.commentHeader}>
+                    {translations.allComments} {data?.total}
+                  </Text>
+                </>
+              }
+              ListFooterComponent={
+                data?.next_page_url === null ? <BottomMessage /> : null
+              }
+              keyExtractor={(_, index) => "" + index}
+              renderItem={({ item }: any) => (
+                <CommentItem
+                  commentID={item.comment_id}
+                  comment={item.comment}
+                  username={item.username}
+                  photo={item.photo}
+                  replies={item.replies}
+                />
+              )}
+              ItemSeparatorComponent={() => (
+                <Divider color="#999" style={styles.divider} />
+              )}
+              onEndReachedThreshold={0.1}
+              onEndReached={onScrollToEnd}
+            />
+            {/* temporary solution for loading - needs improvement */}
+            {(isLoading || isRefetching) && amountOfCommentsToGet > 10 ? (
+              <Loading />
+            ) : null}
+          </>
+        )}
+      </View>
+    );
 };
 
 export default CommentList;
 
 const styles = StyleSheet.create({
-	commentHeader: {
-		color: GLOBAL_COLORS.primaryTextColor,
-		marginBottom: 18,
-	},
-	divider: {
-		marginVertical: 12,
-	},
-	commentsContainer: {
-		padding: 12,
-		flex: 1,
-		minHeight: 100,
-	},
+  commentHeader: {
+    color: GLOBAL_COLORS.primaryTextColor,
+    marginBottom: 18,
+  },
+  divider: {
+    marginVertical: 12,
+  },
+  commentsContainer: {
+    padding: 12,
+    flex: 1,
+    minHeight: 100,
+  },
 });

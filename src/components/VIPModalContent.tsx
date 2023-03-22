89ff2da1d5d46752eeefb7d25d8dc43a9bc4bd36
Mark <mark@1bit.com.ph>
2023-03-22 13:09:19 +0800

    add  translation in single user screen

--- src/components/VIPModalContent.tsx
+++ src/components/VIPModalContent.tsx
@@ -1,30 +1,38 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
-import { GLOBAL_COLORS } from 'global';
-import { VStack, Modal, Text, Button } from 'native-base';
+import { StyleSheet } from "react-native";
+import React from "react";
+import { GLOBAL_COLORS } from "global";
+import { VStack, Modal, Text, Button } from "native-base";
+import { translationStore } from "../zustand/translationStore";
 
-type Props = {}
+type Props = {};
 
 const VIPModalContent = ({ setOpen }) => {
-	return (
-		<Modal.Content bgColor={GLOBAL_COLORS.headerBasicBg}>
-			<Modal.CloseButton />
-			<Modal.Body>
-				<VStack space={8} alignItems="center" margin={0} py={5}>
-					<Text color="white">Upgrade membership first!</Text>
-					<Button size="sm" style={styles.button} onPress={() => setOpen(false)}>Purchase VIP</Button>
-				</VStack>
-			</Modal.Body>
-		</Modal.Content>
-	);
-}
+  const translations = translationStore((state) => state.translations);
+  return (
+    <Modal.Content bgColor={GLOBAL_COLORS.headerBasicBg}>
+      <Modal.CloseButton />
+      <Modal.Body>
+        <VStack space={8} alignItems="center" margin={0} py={5}>
+          <Text color="white">{translations.upgradeMembership}</Text>
+          <Button
+            size="sm"
+            style={styles.button}
+            onPress={() => setOpen(false)}
+          >
+            {translations.purchaseVIP}
+          </Button>
+        </VStack>
+      </Modal.Body>
+    </Modal.Content>
+  );
+};
 
-export default VIPModalContent
+export default VIPModalContent;
 
 const styles = StyleSheet.create({
-	button: {
-		backgroundColor: GLOBAL_COLORS.secondaryColor,
-		borderRadius: 20,
-		width: 120,
-	},
-})
+  button: {
+    backgroundColor: GLOBAL_COLORS.secondaryColor,
+    borderRadius: 20,
+    width: 120,
+  },
+});

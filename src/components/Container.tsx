47ae10fd8a5df65cc29775aed069ceea9f8fe615
Mark <mark@1bit.com.ph>
2023-05-08 13:58:37 +0800

    scale in dimension screen

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,46 @@
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GLOBAL_COLORS } from "global";
 
 interface DataTypes {
   children: any;
 }
 
-const { width } = Dimensions.get("window");
+let scale = Dimensions.get("screen").scale / Dimensions.get("window").scale;
+let screen_height = Dimensions.get("window").height * scale;
+let screen_width = Dimensions.get("window").width * scale;
 
 const Container: React.FC<DataTypes> = ({ children }) => {
-  return <View style={styles.container}>{children}</View>;
+  const [dimensions, setDimensions] = useState({
+    height: screen_height,
+    width: screen_width,
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        const newScale = screen.scale / window.scale;
+        setDimensions({
+          height: window.height * newScale,
+          width: window.width * newScale,
+        });
+      }
+    );
+
+    return () => subscription?.remove();
+  }, []);
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { height: dimensions.height, width: dimensions.width },
+      ]}
+    >
+      {children}
+    </View>
+  );
 };
 
 export default Container;
@@ -18,6 +49,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: GLOBAL_COLORS.primaryColor,
-    width: width,
   },
 });

beaaae75020ebda45655911bfa6ff5699f3ef71b
jestoni-1bit <jestoni@1bit.com.ph>
2023-05-15 15:00:13 +0800

    UI FIX: Upgrade membership display when you click chat in profile even if the user is subscribed in VIP

--- src/screens/SingleUser/index.tsx
+++ src/screens/SingleUser/index.tsx
@@ -12,7 +12,7 @@ import coinIcon from "assets/images/coinIcon.png";
 import CustomModal from "components/CustomModal";
 import DonateModalContent from "components/DonateModalContent";
 import VIPModalContent from "components/VIPModalContent";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import CustomerService from "services/api/CustomerService";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { userStore } from "../../zustand/userStore";
@@ -21,14 +21,23 @@ import UserService from "services/api/UserService";
 const { width } = Dimensions.get("window");
 
 const SingleUserScreen = () => {
+  // ** GLOBAL STORE
   const translations = translationStore((state) => state.translations);
-  const token = userStore((state) => state.api_token);
+  const { api_token, is_Vip } = userStore((store) => store);
+
+  // ** STATES
   const [openVIPModal, setVIPModalOpen] = useState<boolean>(false);
   const [openDonateModal, setOpenDonateModal] = useState<boolean>(false);
-  const [isCreatorFollowed, setIsCreatorFollowed] = useState<boolean | null>(null);
+  const [isCreatorFollowed, setIsCreatorFollowed] = useState<boolean | null>(
+    null
+  );
+
+  // ** HOOKS
+  const navigation = useNavigation<any>();
   const route = useRoute<any>();
   const userID = route?.params?.userID;
 
+  // ** API
   // get specific content creators data
   const { getSpecificContentCreator } = UserService();
   const { data, isFetching } = useQuery({
@@ -36,7 +45,7 @@ const SingleUserScreen = () => {
     queryFn: () =>
       getSpecificContentCreator({
         data: { user_id: userID },
-        token: token,
+        token: api_token,
       }),
     onSuccess: (data) => {
       setIsCreatorFollowed(data?.is_followed);
@@ -73,22 +82,33 @@ const SingleUserScreen = () => {
     },
   });
 
+  // ** EVENTS
   const followContentCreator = () => {
     mutateFollowCreator({
       user_id: { user_id: userID },
-      token: token,
+      token: api_token,
     });
   };
 
   const unfollowContentCreator = () => {
     mutateUnfollowCreator({
       user_id: { user_id: userID },
-      token: token,
+      token: api_token,
     });
   };
-  
+
+  const handleChatPress = () => {
+    if (is_Vip) {
+      navigation.navigate("SingleChatScreen", {
+        postTitle: "CC ID: " + userID,
+      });
+    } else {
+      setVIPModalOpen(true);
+    }
+  };
+
   const tabsData = {
-    Header: () =>
+    Header: () => (
       <SingleUserHeader
         cover_photo={data?.cover_photo}
         photo={data?.photo}
@@ -96,7 +116,8 @@ const SingleUserScreen = () => {
         biography={data?.biography}
         follower_count={data?.follower_count}
         isLoading={isFetching}
-      />,
+      />
+    ),
     tabItems: [
       {
         name: "moment",
@@ -114,26 +135,49 @@ const SingleUserScreen = () => {
   return (
     <React.Fragment>
       <StickyTabs data={tabsData} />
-      {!isFetching &&
+      {!isFetching && (
         <React.Fragment>
-          <HStack style={styles.bottomBar} space={2} justifyContent='center' alignItems='center'>
-            <Pressable onPress={() => setVIPModalOpen(true)} style={{ marginRight: 'auto' }}>
-              <VStack alignItems='center' space={0.5}>
+          <HStack
+            style={styles.bottomBar}
+            space={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Pressable
+              onPress={handleChatPress}
+              style={{ marginRight: "auto" }}
+            >
+              <VStack alignItems="center" space={0.5}>
                 <Image style={styles.image} source={chatInactive} />
                 <Text style={styles.whiteText}>{translations.chat}</Text>
               </VStack>
             </Pressable>
-            <Pressable style={[styles.donateButton, styles.button]} onPress={() => setOpenDonateModal(true)}>
+            <Pressable
+              style={[styles.donateButton, styles.button]}
+              onPress={() => setOpenDonateModal(true)}
+            >
               <HStack space={1.5}>
                 <Image style={styles.image} source={coinIcon} />
                 <Text style={styles.whiteText}>{translations.reward}</Text>
               </HStack>
             </Pressable>
             <Pressable
-              onPress={isCreatorFollowed ? unfollowContentCreator : followContentCreator}
-              style={[styles.button, isCreatorFollowed ? styles.unfollowBG : styles.followBG]}
+              onPress={
+                isCreatorFollowed
+                  ? unfollowContentCreator
+                  : followContentCreator
+              }
+              style={[
+                styles.button,
+                isCreatorFollowed ? styles.unfollowBG : styles.followBG,
+              ]}
             >
-              <Text style={styles.whiteText}>{isCreatorFollowed? '-' : '+'} {isCreatorFollowed ? translations.unfollow : translations.follow}</Text>
+              <Text style={styles.whiteText}>
+                {isCreatorFollowed ? "-" : "+"}{" "}
+                {isCreatorFollowed
+                  ? translations.unfollow
+                  : translations.follow}
+              </Text>
             </Pressable>
           </HStack>
           <CustomModal open={openVIPModal} setOpen={setVIPModalOpen}>
@@ -143,7 +187,7 @@ const SingleUserScreen = () => {
             <DonateModalContent setOpen={setOpenDonateModal} userID={userID} />
           </CustomModal>
         </React.Fragment>
-      }
+      )}
     </React.Fragment>
   );
 };
@@ -155,7 +199,7 @@ const styles = StyleSheet.create({
     backgroundColor: GLOBAL_COLORS.videoContentBG,
     paddingLeft: 34,
     paddingRight: 16,
-    paddingVertical: 12
+    paddingVertical: 12,
   },
   image: {
     height: 20,
@@ -163,23 +207,23 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   whiteText: {
-    color: 'white',
-    fontWeight: 'bold'
+    color: "white",
+    fontWeight: "bold",
   },
   button: {
     width: width < GLOBAL_SCREEN_SIZE.mobile ? 100 : 136,
     height: 28,
     borderRadius: 16,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   donateButton: {
-    backgroundColor: '#FF644A',
+    backgroundColor: "#FF644A",
   },
   followBG: {
-    backgroundColor: GLOBAL_COLORS.secondaryColor
+    backgroundColor: GLOBAL_COLORS.secondaryColor,
   },
-  unfollowBG:{
-    backgroundColor: GLOBAL_COLORS.inactiveTextColor
-  }
-})
+  unfollowBG: {
+    backgroundColor: GLOBAL_COLORS.inactiveTextColor,
+  },
+});
